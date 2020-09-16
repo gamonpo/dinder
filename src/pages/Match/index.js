2b3102be2b8aa5021d80c088e2f4d25013a489f9
gamonpo <gamonpo@outlook.com>
2020-09-16 11:53:53 -0300

    Using data from api

--- src/pages/Match/index.js
+++ src/pages/Match/index.js
@@ -1,85 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {FlatList} from 'react-native';
 
 import {List, Item, Photo, Title} from './styles';
 
 import {Container} from '../../components/Background/styles';
 
+import api from '../../services/api';
+
 import img from '../../assets/pictures/background.png';
-import vegetables from '../../assets/pictures/vegetables.png';
-import bread from '../../assets/pictures/bread.png';
-import fruits from '../../assets/pictures/fruits.png';
-import coffee from '../../assets/pictures/coffee.png';
-import tea from '../../assets/pictures/tea.png';
-import sweet from '../../assets/pictures/sweet.png';
-import spice from '../../assets/pictures/spice.png';
-import egg from '../../assets/pictures/egg.png';
-import meat from '../../assets/pictures/meat.png';
-import cheese from '../../assets/pictures/cheese.png';
 
 export default function Match({navigation}) {
-    const food = [
-        {
-            id: '1',
-            image: vegetables,
-            description: 'Vegetables',
-        },
-        {
-            id: '2',
-            image: fruits,
-            description: 'Fruits',
-        },
-        {
-            id: '3',
-            image: bread,
-            description: 'Bread',
-        },
-        {
-            id: '4',
-            image: sweet,
-            description: 'Sweet',
-        },
-        {
-            id: '5',
-            image: tea,
-            description: 'Tea',
-        },
-        {
-            id: '6',
-            image: cheese,
-            description: 'Cheese',
-        },
-        {
-            id: '7',
-            image: spice,
-            description: 'Spice',
-        },
-        {
-            id: '8',
-            image: coffee,
-            description: 'Coffe',
-        },
-        {
-            id: '9',
-            image: egg,
-            description: 'Egg',
-        },
-        {
-            id: '10',
-            image: meat,
-            description: 'Meat',
-        },
-    ];
+    const [foods, setFoods] = useState([]);
+
+    useEffect(() => {
+        api.get('foods').then((response) => {
+            setFoods(response.data);
+        });
+    });
 
     const renderItem = ({item}) => (
         <Item
             onPress={() => {
                 navigation.navigate('Food', {
                     name: item.description,
-                    image: item.image,
+                    image: item.path,
                 });
             }}>
-            <Photo source={item.image} />
+            <Photo source={{uri: item.path}} />
 
             <Title>{item.description}</Title>
         </Item>
@@ -89,7 +36,7 @@ export default function Match({navigation}) {
         <Container source={img}>
             <List>
                 <FlatList
-                    data={food}
+                    data={foods}
                     renderItem={renderItem}
                     keyExtractor={(item) => item.id}
                     showsVerticalScrollIndicator={false}
