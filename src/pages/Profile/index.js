5457b4b947fa908bf5d6d07387f0883d6f16afe3
gamonpo <gamonpo@outlook.com>
2020-09-16 11:53:12 -0300

    Create profile page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,93 @@
 import React from 'react';
 
-import {Container} from './styles';
+import {Alert} from 'react-native';
 
-const Profile = () => {
-    return <Container />;
-};
+import Icon from 'react-native-vector-icons/Feather';
 
-export default Profile;
+import {
+    Header,
+    HeadTitle,
+    LogOut,
+    Form,
+    Info,
+    Input,
+    Line,
+    Update,
+    BTitle,
+} from './styles';
+
+import {Container} from '../../components/Background/styles';
+
+import img from '../../assets/pictures/background.png';
+
+export default function Profile() {
+    return (
+        <Container source={img}>
+            <Header>
+                <HeadTitle>My profile</HeadTitle>
+                <LogOut onPress={() => Alert.alert('Logout!')}>
+                    <Icon name="log-out" size={24} color="#FFFFFF" />
+                </LogOut>
+            </Header>
+
+            <Form>
+                <Info>
+                    <Icon name="user" size={24} color="#FFFFFF" />
+                    <Input
+                        autoCapitalize="none"
+                        autoCorrect={false}
+                        editable={false}
+                        placeholder=" Name"
+                        placeholderTextColor="#ffffff"
+                    />
+                </Info>
+                <Info>
+                    <Icon name="mail" size={24} color="#FFFFFF" />
+                    <Input
+                        autoCapitalize="none"
+                        autoCorrect={false}
+                        editable={false}
+                        placeholder=" Email"
+                        placeholderTextColor="#ffffff"
+                    />
+                </Info>
+
+                <Line />
+
+                <Info>
+                    <Icon name="lock" size={24} color="#FFFFFF" />
+                    <Input
+                        autoCapitalize="none"
+                        autoCorrect={false}
+                        placeholder=" Current Password"
+                        placeholderTextColor="#ffffff"
+                    />
+                </Info>
+                <Info>
+                    <Icon name="lock" size={24} color="#FFFFFF" />
+                    <Input
+                        autoCapitalize="none"
+                        autoCorrect={false}
+                        secureTextEntry
+                        placeholder=" New Password"
+                        placeholderTextColor="#ffffff"
+                    />
+                </Info>
+                <Info>
+                    <Icon name="lock" size={24} color="#FFFFFF" />
+                    <Input
+                        autoCapitalize="none"
+                        autoCorrect={false}
+                        secureTextEntry
+                        placeholder=" Confirm Password"
+                        placeholderTextColor="#ffffff"
+                    />
+                </Info>
+
+                <Update onPress={() => Alert.alert('Update Sucessfull!')}>
+                    <BTitle>Save changes</BTitle>
+                </Update>
+            </Form>
+        </Container>
+    );
+}
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -1,6 +1,73 @@
 import styled from 'styled-components/native';
+import {Platform, StatusBar} from 'react-native';
 
-export const Container = styled.View`
-    flex: 1;
-    background: tomato;
+const statusBarHeight = Platform.OS === 'android' ? StatusBar.currentHeight : 0;
+
+export const Header = styled.View`
+    padding-top: ${statusBarHeight + 'px'};
+    align-items: center;
+    flex-direction: row;
+    align-self: center;
+    margin: 4px;
+`;
+
+export const HeadTitle = styled.Text`
+    flex-grow: 0.9;
+    font-size: 24px;
+    font-family: Roboto-Regular;
+    color: #ffffff;
+`;
+
+export const LogOut = styled.TouchableOpacity`
+    width: 24px;
+    height: 24px;
+`;
+
+export const Form = styled.View`
+    flex-direction: column;
+    padding-top: ${3 * statusBarHeight + 'px'};
+    align-self: center;
+`;
+
+export const Info = styled.View`
+    background: #4d3f3f3f;
+    width: 350px;
+    height: 50px;
+    margin: 5px;
+    border-radius: 8px;
+    align-items: center;
+    flex-direction: row;
+    padding: 5px;
+`;
+
+export const Input = styled.TextInput`
+    margin-left: 5px;
+    width: 300px;
+    height: 50px;
+    font-family: Roboto-Regular;
+    font-size: 17px;
+    color: #ffffff;
+`;
+
+export const Line = styled.View`
+    height: 1px;
+    background: rgba(255, 255, 255, 1);
+    margin: 20px 0 30px;
+`;
+
+export const Update = styled.TouchableOpacity`
+    width: 350px;
+    height: 50px;
+    background: rgba(233, 102, 41, 1);
+    align-self: center;
+    margin-top: 10px;
+    border-radius: 8px;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const BTitle = styled.Text`
+    font-size: 15px;
+    font-family: Roboto-Regular;
+    color: #ffffff;
 `;
